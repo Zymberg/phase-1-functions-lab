a61037ce97af682688e78d7aec12b0cff2431e59
Granit <gzymeri55@gmail.com>
2022-02-03 12:57:52 +0100

    commit tree d002f842b376c2988d9dc0fe65f672f60b91604a

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(pickupLocation){
+    const headQuearters = 42
+    let distanceByBlocks 
+    distanceByBlocks = Math.abs(pickupLocation - headQuearters)
+    return distanceByBlocks
+}
+
+function distanceFromHqInFeet(pickupLocation){
+    const headQuearters = 42
+    let sqftForBlock = 264
+    let distance
+    distance = Math.abs(pickupLocation - headQuearters)
+    let sqft = distance * sqftForBlock
+    return distance, sqft
+}
+
+function distanceTravelledInFeet(){
+    
+}
